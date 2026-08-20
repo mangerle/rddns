3717b378d3f35e5d34a548875bb335bf9eb0be9f
wangle <wangle@agree.com.cn>
2026-08-20 15:28:56 +0800

    chore(version): 更新项目版本号至0.5.1
    - 修改web-ui显示的版本文本为v0.5.1
    - 更新Cargo.toml中的项目版本为0.5.1
    - 更新Cargo.lock中rddns依赖的版本号为0.5.1

--- web-ui/js/app.js
+++ web-ui/js/app.js
@@ -396,7 +396,7 @@ onLocaleChange(() => {
   }
   const verTextEl = document.getElementById('versionText');
   if (verTextEl) {
-    const currentVer = verTextEl.dataset.version || '0.5.0';
+    const currentVer = verTextEl.dataset.version || '0.5.1';
     verTextEl.innerText = t('common.connected', { version: `v${currentVer}` });
   }
 });
